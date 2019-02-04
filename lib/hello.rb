5d7d32975ae68cf7870d7074e6f18b4c27e96e0d
Jason Fournier <jmf4nier@hotmail.com>
2019-02-04 21:01:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
 
 
 def hello_t(array)
+ if block_given?
   i = 0
  
   while i < array.length
@@ -10,8 +11,7 @@ def hello_t(array)
   array
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+#    puts "Hi, #{name}"
+#  end
+#end
